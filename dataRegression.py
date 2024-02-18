e9b09083fe1f1c23c8ff8bc810d932cfa02122e5
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2024-02-18 11:09:02 -0500

    Update

--- dataRegression.py
+++ dataRegression.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
 melbourne_file_path = 'housing.csv'
@@ -20,17 +24,17 @@ for col in melbourne_data.columns:
             melbourne_data.at[index, col] = 0
 
 # Print the modified DataFrame
-print(melbourne_data)
+#print(melbourne_data)
 
 
 melbourne_data = melbourne_data.dropna(axis=0)
 
 
-melbourne_data.describe
+#melbourne_data.describe
 
 Y = melbourne_data.price
 
-melbourne_features = [    'price',
+melbourne_features = [
     'area',
     'bedrooms',
     'bathrooms',
@@ -53,8 +57,70 @@ melbourne_features = [    'price',
 X = melbourne_data[melbourne_features]
 
 #X.describe
-X.head
+#X.head
 
 melbourne_model = DecisionTreeRegressor(random_state=1)
 
-melbourne_model.fit(X, Y)
+# melbourne_model.fit(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=1)
+
+train_data = X_train.join(y_train)
+
+train_data
+train_data.hist(figsize = (15,8))
+plt.figure(figsize=(15,8))
+sns.heatmap(train_data.corr(), annot=True, cmap="YlGnBu")
+plt.figure(figsize=(15,8))
+sns.scatterplot(x="area", y="price", data=train_data, hue="price", palette="coolwarm")
+from sklearn.linear_model import LinearRegression
+
+X_train, y_train = train_data.drop(['price'], axis=1), train_data['price']
+
+reg = LinearRegression()
+
+reg.fit(X_train, y_train)
+# reg.score(X_train, y_train)
+# reg.predict(X_test)
+
+melbourne_features = [
+    'area',
+    'bedrooms',
+    'bathrooms',
+    'stories',
+    'mainroad',
+    'guestroom',
+    'basement',
+    'hotwaterheating',
+    'airconditioning',
+    'parking',
+    'prefarea',
+    'furnishingstatus']
+
+
+sample = pd.read_csv('housing.csv', 
+                    header=0, 
+                    usecols=["area","bedrooms",	"bathrooms",	"stories",	"mainroad",	"guestroom",	"basement",	"hotwaterheating"	,"airconditioning","parking","prefarea","furnishingstatus"],
+                    nrows=1)
+print(sample[melbourne_features])
+# reg.predict(sample[melbourne_features])
+
+train_data = X_train.join(y_train)
+
+train_data['area']=np.log(train_data['area']+1)
+train_data['bedrooms']=np.log(train_data['bedrooms']+1)
+train_data['bathrooms']=np.log(train_data['bathrooms']+1)
+train_data['stories']=np.log(train_data['stories']+1)
+train_data['price']=np.log(train_data['price']+1)
+# train_data['bedrooms']=np.log(train_data['parking']+1)
+from sklearn.linear_model import LinearRegression
+
+X_train, y_train = train_data.drop(['price'], axis=1), train_data['price']
+
+reg = LinearRegression()
+
+reg.fit(X_train, y_train)
+reg.predict(X_train)
+print("Hello")
+
+# reg.predict(sampleData)
